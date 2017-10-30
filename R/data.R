1e18efe215328356bce88d1d1a7a532a042c3e5f
Guilherme Gall <gmgall@gmail.com>
2017-10-30 02:22:50 -0200

    Inclui dados de exemplo
    Esse commit inclui dados de exemplo que podem ser consumidos pelas funções do
    pacote e suas respectivas documentações.
    
    Foram incluidos:
    
    * coordenadas: um data frame com pontos de ocorrências de 4 espécies
    * mascara: um SpatialPolygonsDataFrame da Mata Atlântica
    * variaveis_preditoras: um RasterStack com variáveis preditoras de exemplo

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Pontos de ocorrência de 4 espécies
+#'
+#' Uma tabela que contém 618 pontos de ocorrência de 4 espécies
+#'
+#' @format Um data frame contendo 618 linhas e 3 colunas:
+#' \describe{
+#'   \item{sp}{Nome da espécie}
+#'   \item{lat}{Latitude}
+#'   \item{lon}{Longitude}
+#' }
+"coordenadas"
+
+#' Máscara para a Mata Atlântica
+#'
+#' @format Um SpatialPolygonsDataFrame da Mata Atlântica
+"mascara"
+
+#' Variáveis preditoras
+#'
+#' @format Um RasterStack com variáveis preditoras de exemplo
+"variaveis_preditoras"
