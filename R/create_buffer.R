6ac10d1fdd9c75eae596629888daec170c5a679f
Andrea Sánchez Tapia <katori@gmail.com>
2018-03-05 15:58:56 -0300

    Calculates distance in km and corrects buffer distance accordingly (x1000)

--- R/create_buffer.R
+++ R/create_buffer.R
@@ -1,26 +1,33 @@
 create_buffer <- function(coord,
                           n.back,
                           buffer.type,
-                          seed,
+                          seed = 512,
                           predictors) {
 
 # Transformando em spatial points
-sp::coordinates(coord) <- ~lon + lat
-raster::crs(coord) <- raster::crs(predictors)
+    sp::coordinates(coord) <- ~lon + lat
+    raster::crs(coord) <- raster::crs(predictors)
     if (buffer.type == "mean")
-        dist.buf <- mean(sp::spDists(x = coord, segments = FALSE))
+        dist.buf <- mean(sp::spDists(x = coord,
+                                     longlat = T,
+                                     segments = FALSE))
     if (buffer.type == "max")
-        dist.buf <- max(sp::spDists(x = coord, segments = FALSE))
+        dist.buf <-  max(sp::spDists(x = coord,
+                                    longlat = T,
+                                    segments = F))
     if (buffer.type == "median")
-        dist.buf <- stats::median(sp::spDists(x = coord, segments = FALSE))
+        dist.buf <- stats::median(sp::spDists(x = coord,
+                                              longlat = T,
+                                              segments = F))
 
     #cria o buffer - é um shape
     buffer.shape <- raster::buffer(coord,
-                                   width = dist.buf * 100,
+                                   width = dist.buf * 1000,
                                    dissolve = TRUE)
 
     #Rasterizando o buffer p/ geração dos ptos aleatorios
-    r_buffer <- raster::rasterize(buffer.shape, predictors,
+    r_buffer <- raster::rasterize(buffer.shape,
+                                  predictors,
                                   field = buffer.shape@plotOrder)
     #o mask é o mínimo necessário para que o buffer fique sem NAs nos preditores
     r_buffer <- raster::mask(r_buffer, predictors[[1]])
