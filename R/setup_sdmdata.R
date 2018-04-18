dcb03cc745e163d6c7192eaf716f6b8cf358da5a
Andrea Sánchez Tapia <katori@gmail.com>
2018-04-18 13:24:48 -0300

    updating documentation/parametrization for sdmdata + clean_na + real_absences

--- R/setup_sdmdata.R
+++ R/setup_sdmdata.R
@@ -1,50 +1,82 @@
 #' Prepares the dataset to perform ENM
 #'
-#' @param species.name A species name
-#' @param coordinates Uma tabela com pontos de ocorrência
-#' @param buffer Define se será usado buffer e de que tipo ("mean" ou "max")
-#' @param seed Para reprodutibilidade
-#' @param predictors Objeto do tipo RasterStack com variáveis preditoras
-#' @param models.dir Path do diretório onde serão escritos os arquivos de saída
-#' @param plot_sdmdata Se o png vai ser criado - defaults to F
-#' @param n.back Número de pontos de background
-#' @return Um data.frame com metadados da modelagem (TSS, AUC, algoritmo etc.)
+#' @param species_name A character string with the species name
+#' @param coordinates A data frame with occurrence data
+#' @param real_absences User defined absence points
+#' @param lon the name of the longitude column. defaults to "lon"
+#' @param lat the name of the latitude column. defaults to "lat"
+#' @param buffer Defines if a buffer will be used to sample pseudo-absences
+#'        (F, "mean", "median", "max")
+#' @param seed For reproducibility purposes
+#' @param predictors A RasterStack of predictor variables
+#' @param clean_nas Logical, delete occurrence points with no environmental
+#' information? Defaults to FALSE and can take a while for large datasets
+#' @param models.dir Folder path to save the output files
+#' @param plot_sdmdata Logical, whether png files will be written
+#' @param n.back Number of pseudoabsence points
+#' @param bootstrap Logical, perform bootstrap
+#' @param boot_proportion Numerical 0 to 1, proportion of points to be sampled
+#' for bootstrap
+#' @param boot_n How many bootstrap runs
+#' @param crossvalidation Logical, perform k-fold crossvalidation
+#' @param cv_partitions Number of partitions in the crossvalidation
+#' @param cv_n How many crossvalidation runs
+#' @return A dataframe called sdmdata with the groups for each run
+#' (in columns called cv.1, cv.2 or boot.1, boot.2), a presence/absence vector,
+#' the geographical coordinates, of the occurrence and pseudoabsence points, and
+#' the associated environmental variables.
+#' @author Andrea Sánchez-Tapia
+#' @seealso \code{\link[dismo]{gridSample}}
+
 #' @export
 #'
 #'
 # tabela de valores
-setup_sdmdata <- function(species.name = species.name,
+setup_sdmdata <- function(species_name = species_name,
                           coordinates = coordinates,
+                          real_absences = NULL,
+                          lon = "lon",
+                          lat = "lat",
                           buffer = FALSE,
                           seed = 512,
                           predictors = predictors,
+                          clean_nas = F,
                           models.dir = models.dir,
                           plot_sdmdata = T,
                           n.back = 1000,
                           bootstrap = F,
                           boot_proportion = 0.8,
-                          n_boot = 10,
+                          boot_n = 10,
                           crossvalidation = F,
-                          partitions = partitions,
-                          n_cv = 10) {
+                          cv_partitions = cv_partitions,
+                          cv_n = 10) {
     if (file.exists(paste0(models.dir)) == FALSE)
         dir.create(paste0(models.dir))
-    if (file.exists(paste0(models.dir, "/", species.name)) == FALSE)
-        dir.create(paste0(models.dir, "/", species.name))
-    partition.folder <- paste0(models.dir, "/", species.name, "/present", "/partitions")
+    if (file.exists(paste0(models.dir, "/", species_name)) == FALSE)
+        dir.create(paste0(models.dir, "/", species_name))
+    partition.folder <- paste0(models.dir, "/", species_name, "/present", "/partitions")
     if (file.exists(partition.folder) == FALSE)
         dir.create(partition.folder, recursive = T)
 
+    coordinates <- coordinates[,c(lon,lat)]
+    names(coordinates) <- c("lon", "lat")
     # tabela de valores
     presvals <- raster::extract(predictors, coordinates)
+    if (clean_nas == TRUE) {
+        coordinates <- coordinates[complete.cases(presvals),]
+        presvals <- presvals[complete.cases(presvals),]
+    }
 
-
+    if (!is.null(real_absences)) {
+        backgr <- real_absences[,c(lon, lat)]
+    } else {
     if (buffer %in% c("mean", "max", "median")) {
         backgr <- create_buffer(coord = coordinates,
                                 n.back = n.back,
                                 buffer.type = buffer,
                                 seed = seed,
                                 predictors = predictors)
+
     } else {
         set.seed(seed + 2)
         backgr <- dismo::randomPoints(mask = predictors,
@@ -52,11 +84,13 @@ setup_sdmdata <- function(species.name = species.name,
                                       p = coordinates,
                                       excludep = T)
     }
+    }
 
     colnames(backgr) <- c("lon", "lat")
 
     # Extraindo dados ambientais dos bckgr
     backvals <- raster::extract(predictors, backgr)
+
     pa <- c(rep(1, nrow(presvals)), rep(0, nrow(backvals)))
 
     pres <- cbind(coordinates, presvals)
@@ -68,37 +102,37 @@ setup_sdmdata <- function(species.name = species.name,
     if (crossvalidation == TRUE) {
         if (nrow(coordinates) < 11) {
             #forces jacknife
-            partitions <- nrow(coordinates)
-            n_cv <- 1
+            cv_partitions <- nrow(coordinates)
+            cv_n <- 1
         }
-        if (n_cv == 1) {
+        if (cv_n == 1) {
             #Crossvalidation
             set.seed(seed)  #reproducibility
-            group <- dismo::kfold(coordinates, partitions)
+            group <- dismo::kfold(coordinates, cv_partitions)
             set.seed(seed + 1)
-            bg.grp <- dismo::kfold(backgr, partitions)
+            bg.grp <- dismo::kfold(backgr, cv_partitions)
             group.all <- c(group, bg.grp)
         }
-        if (n_cv > 1) {
+        if (cv_n > 1) {
             # Repeated CV
-            cv.pres <- replicate(n = n_cv,
-                                 dismo::kfold(coordinates, partitions))
-            dimnames(cv.pres) <- list(NULL, paste0("cv", 1:n_cv))
-            cv.back <- replicate(n = n_cv,
-                                 dismo::kfold(backgr, partitions))
-            dimnames(cv.back) <- list(NULL, paste0("cv", 1:n_cv))
+            cv.pres <- replicate(n = cv_n,
+                                 dismo::kfold(coordinates, cv_partitions))
+            dimnames(cv.pres) <- list(NULL, paste0("cv", 1:cv_n))
+            cv.back <- replicate(n = cv_n,
+                                 dismo::kfold(backgr, cv_partitions))
+            dimnames(cv.back) <- list(NULL, paste0("cv", 1:cv_n))
             cv.matrix <- rbind(cv.pres, cv.back)
         }
     }
     # Bootstrap
     if (bootstrap == TRUE) {
-    boot.pres <- replicate(n = n_boot,
+    boot.pres <- replicate(n = boot_n,
                            sample(
                                x = seq_along(1:nrow(coordinates)),
                                size = nrow(coordinates) * boot_proportion,
                                replace = FALSE
                            ))
-    boot.back <- replicate(n = n_boot,
+    boot.back <- replicate(n = boot_n,
                            sample(
                                x = seq_along(1:nrow(backgr)),
                                size = nrow(backgr) * boot_proportion,
@@ -106,16 +140,16 @@ setup_sdmdata <- function(species.name = species.name,
                            ))
     boot_p <- matrix(data = "test",
                      nrow = nrow(coordinates),
-                     ncol = n_boot,
-                     dimnames = list(NULL,paste0("boot",1:n_boot)))
+                     ncol = boot_n,
+                     dimnames = list(NULL,paste0("boot",1:boot_n)))
     boot_a <- matrix(data = "test",
                      nrow = nrow(backgr),
-                     ncol = n_boot,
-                     dimnames = list(NULL,paste0("boot",1:n_boot)))
-    for (i in seq_along(1:n_boot)) {
+                     ncol = boot_n,
+                     dimnames = list(NULL,paste0("boot",1:boot_n)))
+    for (i in seq_along(1:boot_n)) {
         boot_p[, i][boot.pres[, i]] <- "train"
         }
-    for (i in seq_along(1:n_boot)) {
+    for (i in seq_along(1:boot_n)) {
         boot_a[, i][boot.back[, i]] <- "train"
     }
     boot.matrix <- rbind(boot_p, boot_a)
@@ -131,7 +165,7 @@ setup_sdmdata <- function(species.name = species.name,
     if (plot_sdmdata) {
         #Creates a .png plot of the initial dataset
         cat(paste("Plotting the dataset...",'\n'))
-        png(filename = paste0(partition.folder, "/sdmdata_", species.name,".png"))
+        png(filename = paste0(partition.folder, "/sdmdata_", species_name,".png"))
         par(mfrow = c(1, 1), mar = c(5, 4, 3, 0))
         raster::plot(predictors[[1]], legend = F, col = "grey90", colNA = NA)
         points(back, pch = ".", col = "black")
