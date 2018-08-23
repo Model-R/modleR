bc6e93cbe34aa09cb7ff61649ad1631801587ce2
Andrea Sánchez Tapia <katori@gmail.com>
2018-08-23 18:25:06 -0300

    setup -> set seed to null - ensemble: scale models to max. passing checks.

--- R/ensemble.R
+++ R/ensemble.R
@@ -28,6 +28,8 @@
 #' @param consensus_level Threshold for the consensus rule, betwen 0 and 1
 #'                        (0.5 means a majority rule).
 #' @param write_png Write png? Defaults to TRUE
+#' @param scale_models Sets the maximum value of the input models to 1.
+#' Defaults to TRUE.
 #' @param write_raw_map Create a mean raw map without margins
 #'
 #' @import raster
@@ -48,6 +50,7 @@ ensemble_model <- function(species_name,
                            consensus = FALSE,
                            consensus_level = 0.5,
                            write_png = T,
+                           scale_models = TRUE,
                            write_raw_map = F) {
 
 
@@ -70,29 +73,30 @@ ensemble_model <- function(species_name,
             cat(paste(length(tif.files), whi,
                       "models to ensemble from for", species_name, "\n"))
             mod2 <- raster::stack(tif.files)
-            #if (length(tif.files) == 1) {
-             #   ensemble.mean <- mod2
-            #} else {
-                ensemble.mean <- raster::overlay(mod2, fun = function(x) {
-                    return(mean(x, na.rm = T))
-                    }
-                    )
-                ensemble.sd <- raster::overlay(mod2, fun = function(x) {
-                    return(sd(x, na.rm = T))
-                    }
-                    )
-                #ensemble.min <- raster::overlay(mod2, fun = function(x) {
-                 #   return(min(x, na.rm = T))
-                  #  }
-                   # )
-                #ensemble.max <- raster::overlay(mod2, fun = function(x) {
-                 #   return(max(x, na.rm = T))
-                  #  }
-                   # )
-                ensemble.median <- raster::overlay(mod2, fun = function(x) {
-                    return(stats::median(x, na.rm = T))
-                    }
-                    )
+            #scale models to 0-1
+            if (scale_models == T) {
+                mod2 <- mod2/raster::maxValue(mod2)
+            }
+            ensemble.mean <- raster::overlay(mod2, fun = function(x) {
+                return(mean(x, na.rm = T))
+                }
+                )
+            ensemble.sd <- raster::overlay(mod2, fun = function(x) {
+                return(sd(x, na.rm = T))
+                }
+                )
+            #ensemble.min <- raster::overlay(mod2, fun = function(x) {
+             #   return(min(x, na.rm = T))
+              #  }
+               # )
+            #ensemble.max <- raster::overlay(mod2, fun = function(x) {
+             #   return(max(x, na.rm = T))
+              #  }
+               # )
+            ensemble.median <- raster::overlay(mod2, fun = function(x) {
+                return(stats::median(x, na.rm = T))
+                }
+                )
             ensemble.mods <- raster::stack(ensemble.mean,
                                            ensemble.median,
                                            ensemble.sd#,
@@ -114,6 +118,32 @@ ensemble_model <- function(species_name,
                     res = 300, width = 410 * 300 / 72, height = 480 * 300 / 72)
                 par(mfrow = c(1, 1), mar = c(4, 4, 0, 0))
                 raster::plot(ensemble.mean)
+                maps::map("world",
+                          c("", "South America"),
+                          add = T,
+                          col = "grey")
+                points(coord, pch = 21, cex = 0.6,
+                       bg = scales::alpha("cyan", 0.6))
+                dev.off()
+                png(filename = paste0(models_dir, "/", species_name, "/present/",
+                                      ensemble_dir, "/", species_name, "_", whi,
+                                      "_ensemble_median.png"),
+                    res = 300, width = 410 * 300 / 72, height = 480 * 300 / 72)
+                par(mfrow = c(1, 1), mar = c(4, 4, 0, 0))
+                raster::plot(ensemble.median)
+                maps::map("world",
+                          c("", "South America"),
+                          add = T,
+                          col = "grey")
+                points(coord, pch = 21, cex = 0.6,
+                       bg = scales::alpha("cyan", 0.6))
+                dev.off()
+                png(filename = paste0(models_dir, "/", species_name, "/present/",
+                                      ensemble_dir, "/", species_name, "_", whi,
+                                      "_ensemble_sd.png"),
+                    res = 300, width = 410 * 300 / 72, height = 480 * 300 / 72)
+                par(mfrow = c(1, 1), mar = c(4, 4, 0, 0))
+                raster::plot(ensemble.sd)
                 maps::map("world",
                           c("", "South America"),
                           add = T,
