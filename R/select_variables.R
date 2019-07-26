1e2b9746dde04203b64f8995315882319f19ad83
saramortara <saramortara@gmail.com>
2019-07-26 16:57:04 -0300

    fix inherit in select_variables

--- R/select_variables.R
+++ R/select_variables.R
@@ -1,9 +1,11 @@
 #' Helper function to select environmental variables from a stack.
 #'
-#' This function takes a stack of environmental variables and the calibration area. The function calculates Pearson correlations between all environmental variables and returns a new stack with uncorrelated variables based on a cutoff.
+#' This function takes a stack of environmental variables and the calibration area. 
+#' The function calculates Pearson correlations between all environmental variables and 
+#' returns a new stack with uncorrelated variables based on a cutoff.
 #'
-#' @param species_name A character string with the species name
-#' @param predictors Stack of environmental variables
+#' @inheritParams setup_sdmdata
+#' @inheritParams create_buffer
 #' @param buffer Raster specified by the user or output from create_buffer()
 #' @param cutoff Cutoff value of correlation between variables to exclude environmental layer. 
 #' Default is to exclude environmental variables with correlation > 0.8
@@ -20,15 +22,17 @@
 #' @examples
 #'
 #' ## selecting data for only sp1
-#' coord1sp <- coordenadas[coordenadas$sp == unique(coordenadas$sp)[1],]
+#' coord1sp <- coordenadas[[1]]
 #' ## selecting only columns with longitude and latitude
 #' occ <- coord1sp[,c(2,3)]
-#' # using coord1sp to create buffer w/ mean distance between points
-#' buf <- create_buffer("foo", occ, example_vars)
-#' # running select_variables w/ output from create_buffer
-#' select_variables("foo", predictors = example_vars, buffer = buf)
-select_variables <- function(species_name,
-                             predictors,
+#' ## using coord1sp to create buffer w/ mean distance between points
+#' buf <- create_buffer("foo", occ, predictors=example_vars)
+#' ## running select_variables w/ output from create_buffer
+#' select_variables(predictors = example_vars, buffer = buf)
+#' 
+#' ## selecting variables for the entire area with a different cutoff
+#' select_variables(example_vars, cutoff=0.5)
+select_variables <- function(predictors,
                              buffer = NULL,
                              cutoff = 0.8,
                              percent = 0.8,
@@ -46,9 +50,9 @@ select_variables <- function(species_name,
       n = floor(sum(!is.na(raster::values(predictors[[1]]))) * percent))
   vals <- raster::extract(x = predictors, sampled)
   vals <- vals[complete.cases(vals),]
-  exclude.vars <- caret::findCorrelation(cor(vals), cutoff = cutoff)
-  if (length(exclude.vars) > 0) {
-      excluded <- names(predictors)[exclude.vars]
+  exclude_vars <- caret::findCorrelation(cor(vals), cutoff = cutoff)
+  if (length(exclude_vars) > 0) {
+      excluded <- names(predictors)[exclude_vars]
       retained <- setdiff(names(predictors), excluded)
       final_vars <- raster::subset(predictors, retained, drop = F)
       message(paste(paste(excluded, collapse = ","),
